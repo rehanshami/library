869a5afd68c6be5419a3cbb28f8ffef769bd5c1d
rehanshami <77270784+rehanshami@users.noreply.github.com>
2023-07-31 00:28:59 +1200

    Implement logic to add books and toggle book input form

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#isRead');
 const submit = document.querySelector('#button');
 
+
 submit.addEventListener('click', addBookToLibrary);
 
-let myLibrary = [  {
+let myLibrary = [{
   title: 'The Hobbit',
   author: 'J.R.R Tolkien',
   pages: 303,
@@ -33,6 +34,7 @@ function addBookToLibrary(event) {
 
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
+  displayBooksInLibrary(myLibrary);
   clearBookForm();
 
 }
@@ -44,4 +46,54 @@ function clearBookForm() {
   readInput.checked = false;
   
 }
+
+function displayBooksInLibrary() {
+  const library = document.querySelector('.library-books');
+
+  const booksInLibrary = document.querySelector('.library-books .book');
+  myLibrary.forEach((book) => {
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    const bookTitle = document.createElement('p');
+    bookTitle.textContent = `Title: ${book.title}`
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = `Author: ${book.author}`;
+
+    const bookPages = document.createElement('p');
+    bookPages.textContent = `Pages: ${book.pages}`
+
+    const bookRead = document.createElement('p');
+    bookRead.textContent = `Read: ${book.read ? "Yes" : "No"}`;
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+
+    booksInLibrary.appendChild(bookCard);
+    library.appendChild(booksInLibrary);
+
+
+
+  })
+}
 console.log(myLibrary)
+
+function myFunction(item) {
+  console.log(item)
+  const bookTitle = document.createElement('p');
+  bookTitle.textContent = this.title.value;
+  book.appendChild(bookTitle);
+  }
+
+  displayBooksInLibrary();
+
+
+const bookForm = document.querySelector('.book-form');
+const addBookInput = document.querySelector('#add-book');
+addBookInput.addEventListener('click', () => {
+  bookForm.classList.toggle('display-toggle');
+  })
